2f8c6f3a778a5451c5755903131d421271983e06
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-26 17:28:23 +0530

    chalk package added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
 //Fetch Location:  https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 const input = process.argv;
 const printMeaning = async(word)=>{
@@ -9,15 +10,16 @@ try{
         );
         const data = await response.json();
 
-        const meaning = data[0].meanings.map((meaning)=>{
-        console.log(meaning.definitions[0].definition);
+        data[0].meanings.map((meaning)=>{
+        console.log(chalk.blue(meaning.definitions[0].definition));
         });
 }
 else{
-    console.log("Please Enter the Keyword to know the meaning")
+    console.log(chalk.red("Please enter a word to know the meaning"));
 }
 }catch(error){
-console.log("Meaning Not Found, \n(1)Kindly check spelling and Try again\n(2)Don't Enter empty words")
+console.log(chalk.red("Meaning Not Found, \n(1)Kindly check spelling and Try again\n(2)Don't Enter empty words"));
 }
 }
-printMeaning(input[2])
+printMeaning(input[2]);
+export default printMeaning;
